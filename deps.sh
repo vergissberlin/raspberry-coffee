6bd5025c8e6f64be908b025befc2a8c835067f71
André Lademann <andre.lademann@netresearch.de>
2017-01-06 17:07:23 +0100

    Added debounce node

--- deps.sh
+++ deps.sh
@@ -11,7 +11,17 @@ set -o errexit
 
 # Install dependencies
 apt-get update &&\
-apt-get install git-core python-rpi.gpio nodered wireless-tools firmware-ralink sox libsox-fmt-all festival festival-doc festival-freebsoft-utils
+apt-get install \
+    git-core \
+    python-rpi.gpio \
+    nodered \
+    wireless-tools \
+    firmware-ralink \
+    sox \
+    libsox-fmt-all \
+    festival \
+    festival-doc \
+    festival-freebsoft-utils
 
 # Update node
 npm install -g npm@2.x
@@ -25,4 +35,11 @@ npm config set unsafe-perm true -g --unsafe-perm
 
 # Install additional nodes
 cd /usr/src/app/node-red/
-npm i node-red-node-pibrella node-red-contrib-resinio node-red-node-feedparser node-red-node-xmpp node-red-contrib-say node-red-contrib-play
+npm i \
+    node-red-node-pibrella \
+    node-red-contrib-resinio \
+    node-red-node-feedparser \
+    node-red-node-xmpp \
+    node-red-contrib-say \
+    node-red-contrib-play \
+    node-red-contrib-debounce
