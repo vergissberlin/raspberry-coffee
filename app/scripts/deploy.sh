ced8362b588ea0230801ff9ffa677ddccb9eb2c2
André Lademann <vergissberlin@googlemail.com>
2017-01-20 23:27:50 +0100

    Changed start script to recover newser flows

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -18,7 +18,7 @@ cat /usr/src/app/app/node-red/flows.json > app/node-red/flows.json &&\
 
 # 3. Add and commit changes
 git add app/node-red/flows.json &&\
-git commit -am "Deploy new coffee-bin flow from Raspberry PI" >> /data/deploy.log &&\
+git commit -am "Deploy new coffee-bin flow from Raspberry PI" >> /data/log/deploy.log &&\
 
 # 4. Push and log
-git push -u >> /data/log/deploy.log
+git push -u >> /data/log/push.log
--- start.sh
+++ start.sh
@@ -22,8 +22,9 @@ cp -n /data/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 cp -n /data/ssh/id_rsa ~/.ssh/id_rsa
 ssh-keyscan github.com > ~/.ssh/known_hosts
 
-# Restore credentials
-cp -n /data/node-red/flows_cred.json /usr/src/app/app/node-red/flows_cred.json
+# Restore flows and credentials if newer
+cp -u /data/node-red/flows.json /usr/src/app/app/node-red/flows.json
+cp -u /data/node-red/flows_cred.json /usr/src/app/app/node-red/flows_cred.json
 
 # Link pm2 to keymetrics API and name instance resin DEVICE ID
 pm2 link $SECRET_KEY $PUBLIC_KEY $RESIN_DEVICE_UUID
